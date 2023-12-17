bfd93fdfb712bb6f22bc11bedc3cae610e28e0c8
Luke Park <143543147+lukep258@users.noreply.github.com>
2023-12-17 00:30:53 -0600

    postgres fix

--- server.js
+++ server.js
@@ -26,17 +26,16 @@ const testGet=()=>{
 
 const newPool=()=>{
     const connectionString=process.env.DATABASE_URL||{
-        user:process.env.DB_USER,
+        user: process.env.DB_USER,
         host: process.env.DB_HOST,
         database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT
     }
-    const pool=new pg.Pool(connectionString)
+
+    const pool=new pg.Pool({connectionString})
     return pool
 }
 
 const pool = newPool()
-pool.query('select * from players')
-.then(result=>(console.log(result.rows)))
 init()
